8df800c88fac77cdb253b31b321b0c911fe4e0d2
Bruno <Bruno_camargoSC@hotmail.com>
2022-12-19 21:10:21 -0300

    Last commit project

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 //importar o express
-const express = require('express');
+const express = require("express");
 
 const routes = require("./routes");
 
@@ -9,6 +12,24 @@ app.use(express.json());
 
 app.use(routes);
 
-// uma constante com o numero da porta "endereço" que a API vai ficar observando 
+app.use((error, request, response, next) => {
+  //Erro do lado do Cliente
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  //Erro do lado do Servidor
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+// uma constante com o numero da porta "endereço" que a API vai ficar observando
 const PORT = 3333;
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
