352374e1be72ab8c40a6f01e199adca4cb6d403c
Bruno <Bruno_camargoSC@hotmail.com>
2023-01-04 20:33:15 -0300

    new commit

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 class UsersController {
@@ -13,13 +14,51 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     //quero inserir na tabela de usuario =>
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password]
+      [name, email, hashedPassword]
     );
 
     return response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [íd]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já esta em uso. ");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(
+      `
+  UPDATE users SET
+  name = ?,
+  email = ?,
+  updated-at = ?
+  WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    );
+
+    return response.json();
   }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,4 +8,6 @@ const usersController = new UsersController();
 
 userRoutes.post("/", usersController.create);
 
+userRoutes.put("/:id,", usersController.update);
+
 module.exports = userRoutes;
